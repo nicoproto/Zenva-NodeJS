ef8935f05d6a41f57d9b30ad193c925dfbd8f1c3
Nicolás Proto <nicolasproto@gmail.com>
2019-05-22 16:25:52 -0300

    Setting up Node.js and Hello world exercise

---
+++ 01-hello-world/hello.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+function serverCallback(req, res) {
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello ' + process.argv[2]);
+}
+
+http.createServer(serverCallback).listen(8080);
